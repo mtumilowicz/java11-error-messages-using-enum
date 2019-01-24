78c1150c5407dc28df5cb2afb4f1d649976dc907
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-24 21:30:49 +0100

    RegExPattern.java messages

--- src/main/java/RegExPattern.java
+++ src/main/java/RegExPattern.java
@@ -7,7 +7,10 @@ import java.util.regex.Pattern;
  * Created by mtumilowicz on 2019-01-24.
  */
 enum RegExPattern {
-    EMAIL("[\\w._]+@[\\w.]+\\.[\\w]{2,}", "It is not a proper email."),
+    EMAIL("[\\w.]+@[\\w.]+\\.[\\w]{2,}", "Email pattern: address@prefix.suffix, where:" +
+            "address contains letters, numbers, underscores and dots, " +
+            "prefix contains letters, numbers, underscores and dots, " +
+            "suffix is contains at least 2 letters, numbers and underscores."),
     POSTAL_CODE("\\d{2}-\\d{3}", "Postal code pattern: XX-XXX, where X is a digit."),
     WORD("[\\w]+", "Word could contain only letters, numbers and underscores.");
 
